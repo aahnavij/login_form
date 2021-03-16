d4aa37fc7a891a6ad57089c58dba08643f866255
aahnavij <80687775+aahnavij@users.noreply.github.com>
2021-03-16 18:50:42 +0530

    first commit

--- insert.php
+++ insert.php
@@ -5,8 +5,36 @@ extract($_POST);
 if(isset($_POST['submit'])){
 
 
+// 	 $fname = $_POST['fname'];
+//  $lname = $_POST['lname'];
+//  $email = $_POST['email'];
+//  $phone = $_POST['phone'];
+//  $address  = $_POST['address'];
+//   $birthday = $_POST['birthday'];
+//    $gender = $_POST['gender'];
+//     $state = $_POST['state'];
+//      $zip = $_POST['zip'];
+//       $company = $_POST['company'];
+
+
+// if($email != "") {
+// $q = mysqli_query("select * from `ajaxlogin` where email='".$email."'");
+// $fetch = mysql_num_rows($q);
+// if($fetch>0)
+// {
+// echo "email already inserted";
+// }
+// else
+// {
+// $sql = mysqli_query("INSERT INTO ajaxlogin (email) VALUES ('$email')");
+// echo "Thank you for Submitting. Redirecting back to Home Page";
+// }}
+
+
 	$q="insert into ajaxlogin (first_name,last_name,Email,phone_no,Address,birthday,gender,state,zip_code,company) values ('$fname','$lname','$email','$phone','$address','$birthday','$gender','$state','$zip','$company')";
 	echo '<script>alert("Success")</script>'; 
+
+	echo $q;
 	$query=mysqli_query($con,$q);
 	// header('location:iinsert.php');
 
--- login_form.php
+++ login_form.php
@@ -5,25 +5,34 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 </head>
 <body>
-<form id="myform" action="insert.php" method="post">
+<form id="myform" action="insert.php" method="post" onsubmit="return validation()">
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value=""><br>
+  <input type="text" id="fname" name="fname" value="" required><br>
+
+    <button id="okButton" disabled>OK</button>
+
+  <span id="firstn"></span><br><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="">
+  <input type="text" id="lname" name="lname" value="" required>
 <br>
+ <span id="lastn"></span><br><br>
  <label for="email">Enter your email:</label>
   <br>
   <input type="email" id="email" name="email">
 <br>
+<span id="email_id"></span><br>
  <label for="phone">Enter a phone number:</label><br><br>
-  <input type="tel" id="phone" name="phone" placeholder="123-45-678" ><br><br>
+  <input type="tel" id="phone" name="phone" placeholder="123-45-678" required><br><br>
   <small>Format: 123-45-678</small><br><br>
+  <span id="mobile_no"></span><br><br>
 
   <label >Address:</label><br>
-  <input type="text" id="address" name="address" ><br><br>
+  <input type="text" id="address" name="address" required><br><br>
+  <span id="address_f"></span><br><br>
 
   <label for="birthday">Birthday:</label><br>
-  <input type="date" id="birthday" name="birthday"><br><br>
+  <input type="date" id="birthday" name="birthday" required><br><br>
+
 
 
   <label for="gender">gender</label><br>
@@ -40,14 +49,17 @@
  
 
   <label for="state">State:</label><br>
-  <input type="text" id="state" name="state"><br><br>
+  <input type="text" id="state" name="state" required><br><br>
+  <span id="state_f"></span><br><br>
 
 <label >Enter zip code</label><br>
-  <input type="text" id="zip" name="zip" ><br><br>
+  <input type="text" id="zip" name="zip" required><br><br>
+  <span id="zip_code"></span><br><br>
   
   
   <label >Enter your company name:</label><br>
-  <input type="text" id="company" name="company" ><br><br>
+  <input type="text" id="company" name="company" required><br><br>
+  <span id="company_name"></span><br><br>
 
 <input type="submit" id="submit" name="submit" value="submit">
 </form>
@@ -68,6 +80,115 @@ $(document).ready(function(){
   });
 
 });
-	</script>
+// function fuu()
+// {
+// 	alert('wrong');
+// }
+
+
+
+
+const nameField = document.getElementById('fname');
+const isValidEmail = emailField.checkValidity();
+nameField.addEventListener('keyup', function (event) {
+  isValidEmail = emailField.checkValidity();
+  
+  if ( isValidEmail ) {
+    okButton.disabled = false;
+  } else {
+    okButton.disabled = true;
+  }
+});
+
+
+
+
+
+
+		
+function validation(){
+
+var first_n = document.getElementById('fname').value;
+if(first_n == "")
+{
+	document.getElementById('firstn').innerHTML="please fill the name";
+
+	return false;
+}
+if(!isNaN(first_n)){
+	document.getElementById('firstn').innerHTML="only characters are allowed";
+	document.getElementById('firstn').style.color="red";
+	return false;
+}
+var last_n = document.getElementById('lname').value;
+
+
+if(last_n == "")
+{
+	document.getElementById('lastn').innerHTML="please fill the name";
+	return false;
+}
+if(!isNaN(last_n)){
+	document.getElementById('lastn').innerHTML="only characters are allowed";
+		document.getElementById('lastn').style.color="red";
+	return false;
+}
+
+var mobile_number = document.getElementById('phone').value;
+
+if(mobile_number == "")
+{
+	document.getElementById('mobile_no').innerHTML="please fill the mobile number";
+	return false;
+}
+if(isNaN(mobile_number)){
+	document.getElementById('mobile_no').innerHTML="user must write digits only";
+		document.getElementById('mobile_no').style.color="red";
+	return false;
+}
+
+if(mobile_number.length<10 || mobile_number.length>15){
+	document.getElementById('mobile_no').innerHTML="enter valid mobile number";
+
+		document.getElementById('mobile_no').style.color="red";
+	return false;
+}
+var address_field = document.getElementById('address').value;
+if(address_field == ""){
+	document.getElementById('address_f').innerHTML="please fill out address";
+	return false;
+
+}
+ 
+ if(!isNaN(address_field)){
+ 	document.getElementById('address_f').innerHTML="invalid address";
+ 	return false;
+ }
+
+
+var state_field = document.getElementById('state').value;
+if(!isNaN(state_field)){
+	document.getElementById('state_f').innerHTML="invalid state";
+}
+
+
+
+var email_field = document.getElementById('email');
+if(email_field==""){
+	document.getElementById('mobile_no').innerHTML="please enter email";
+		document.getElementById('mobile_no').style.color="red";
+	return false;
+}
+if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email_field));
+  {
+    return (true);
+  }
+
+    alert("You have entered an invalid email address!")
+    	document.getElementById('firstn').style.color="red";
+    return (false);
+
+}
+</script>
 </body>
 </html>
